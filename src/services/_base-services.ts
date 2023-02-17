aa7e6a13cc561859f5b087cb0ae75ab8373ddf79
edwardedo603 <edwardedo603@gmail.com>
2023-02-17 08:28:29 +0700

    fix: 🐛 cache header axios

--- src/services/_base-services.ts
+++ src/services/_base-services.ts
@@ -1,14 +1,10 @@
 import axios from 'axios'
-import { setupCache } from 'axios-cache-adapter'
-const cache = setupCache({
-  maxAge: 10 * 1000,
-})
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL
 
 const service = axios.create({
   baseURL,
-  adapter: cache.adapter,
+  headers: { 'Cache-Control': 'no-cache' },
 })
 
 export default service
