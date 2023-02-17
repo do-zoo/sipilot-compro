8ce3c2556d88b8e7ae01274640288f94b357ce60
edwardedo603 <edwardedo603@gmail.com>
2023-02-17 08:20:21 +0700

    fix: 🐛 axios cache

--- src/services/_base-services.ts
+++ src/services/_base-services.ts
@@ -1,9 +1,14 @@
 import axios from 'axios'
+import { setupCache } from 'axios-cache-adapter'
+const cache = setupCache({
+  maxAge: 10 * 1000,
+})
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL
 
 const service = axios.create({
   baseURL,
+  adapter: cache.adapter,
 })
 
 export default service
