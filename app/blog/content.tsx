a1ec9e62d83b457a6c29a152cbaaf60bad908804
edwardedo603 <edwardedo603@gmail.com>
2023-02-17 07:11:05 +0700

    fix: 🐛 photo on random post

--- app/blog/content.tsx
+++ app/blog/content.tsx
@@ -42,8 +42,8 @@ export default function BlogContent(props: IProps) {
               bg="transparent"
             >
               <Image
-                src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
-                alt="trusted"
+                src={random.thumbnail[0]}
+                alt="thumbnail"
                 fill
                 style={{
                   objectFit: 'cover',
