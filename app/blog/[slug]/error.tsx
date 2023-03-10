25c58392ede2c4294d0f84419b8826e279ab1b7e
edwardedo603 <edwardedo603@gmail.com>
2023-03-11 03:57:06 +0700

    feat: create custom server for cpanel hosting

--- app/blog/[slug]/error.tsx
+++ app/blog/[slug]/error.tsx
@@ -1,18 +1,15 @@
 'use client' // Error components must be Client components
 
-import { redirect } from 'next/navigation'
+import Loader from '@sipilot/components/Loader'
+import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 
-export default function Error({}: //   error,
-//   reset,
-{
-  error: Error
-  reset: () => void
-}) {
+export default function Error({}: { error: Error; reset: () => void }) {
+  const router = useRouter()
   useEffect(() => {
     // Log the error to an error reporting service
-    redirect('/blog')
-  }, [])
+    router.replace('blog')
+  }, [router])
 
-  return null
+  return <Loader />
 }
--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -3,19 +3,14 @@ import { blogServices } from '@sipilot/services/blog/blog.services'
 import { Suspense } from 'react'
 import BlogSlugContent from './content'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Page({ params }: { params: { slug: string } }) {
-  const blog = await blogServices.getBySlug(params?.slug)
+  const blog = await blogServices.getBySlug(params.slug)
+
   return (
     <Suspense fallback={<Loader />}>
       <BlogSlugContent data={blog} />
     </Suspense>
   )
 }
-
-// export async function generateStaticParams() {
-//   const posts = await blogServices.getAll();
-
-//   return posts.map((post) => ({
-//     slug: post.slug,
-//   }));
-// }
